c78fef96f63591bdf3be4bf8d5c13de7a8d464ac
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-10-25 19:38:03 +0100

    Entities Update

--- Domain/Entities/SchoolClass.cs
+++ Domain/Entities/SchoolClass.cs
@@ -1,13 +1,17 @@
 ﻿using Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Domain.Entities
 {
     public class SchoolClass : BaseEntity
     {
-        // Props    
+        // Props  
+        [StringLength(5, MinimumLength = 3)] // Data Field Max 30 and min 3 chars
+        [Column(TypeName = "nvarchar(10)")]
         public string ClassName { get; set; }
 
 
