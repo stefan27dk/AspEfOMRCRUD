37cd8d3913b39df6695db1b3929b34820ced94ac
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-10-26 18:07:16 +0100

    Folder rearrangement

---
+++ Application/Services/StudentServices/StudentCommands/CreateStudentCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Application.Services.Command
+{
+    public class CreateStudentCommand : IRequest<int>
+    {
+        // Props
+        [StringLength(30, MinimumLength = 3)] // Data Field Max 30 and min 3 chars
+        [Column(TypeName = "nvarchar(30)")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] // Only Leters allowed
+        public string FirstName { get; set; }
+    }
+}
