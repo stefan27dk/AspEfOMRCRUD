e748d58737d03656e936a5a569c761ac8a9640ea
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-11-17 19:33:09 +0100

    Added CreateRole

---
+++ Web/Controllers/AdministrationController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.ViewModels.RolesViewModels;
+
+namespace Web.Controllers
+{
+
+    // Class ============= || AdministrationController ||==========================================
+    public class AdministrationController : Controller
+    {
+
+        // RoleManager
+        private readonly RoleManager<IdentityRole> roleManager;
+
+
+
+        //|| Constructor ||
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+
+
+
+
+        // Create Role - || Get ||
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+
+
+
+        // Create Role - || Post ||
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole { Name = model.RoleName };  // New Role  
+                IdentityResult result = await roleManager.CreateAsync(identityRole); // Insert / Create the Role
+
+                // If OK
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                  
+                // If Error - Add errors to modelstate
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+             
+            return View(model); // Return model with the Errors from the foreach loop
+        }
+    }
+}
