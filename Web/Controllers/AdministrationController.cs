d9e8103ee8984b9ed6f45c8e315212711f1ba304
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-11-18 14:07:58 +0100

    Added - Edit Role

--- Web/Controllers/AdministrationController.cs
+++ Web/Controllers/AdministrationController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Models;
 using Web.ViewModels.RolesViewModels;
 
 namespace Web.Controllers
@@ -13,15 +15,17 @@ namespace Web.Controllers
     public class AdministrationController : Controller
     {
 
-        // RoleManager
-        private readonly RoleManager<IdentityRole> roleManager;
+        
+        private readonly RoleManager<IdentityRole> roleManager; // RoleManager
+        private readonly UserManager<ApplicationUser> userManager; // User Manager
 
 
 
         //|| Constructor ||
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
 
@@ -58,11 +62,12 @@ namespace Web.Controllers
                 {
                     ModelState.AddModelError("", error.Description);
                 }
-            }
-
-             
+            }    
+            
             return View(model); // Return model with the Errors from the foreach loop
         }
+
+
 
 
 
@@ -78,5 +83,79 @@ namespace Web.Controllers
         }
 
 
+
+
+
+
+       // EditRole - || GET ||
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string Id)
+        {  
+            var role = await roleManager.FindByIdAsync(Id); // Find Role    
+            if(role==null) // if role dont exists
+            {
+                ViewBag.ErrorMessage = $"Role with ID = {Id} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel { Id = role.Id, RoleName = role.Name }; // ViewModel
+
+            foreach (var user in await userManager.Users.ToListAsync()) // Loop users
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name)) // if user have the role add it to the UsersList in the ViewModel
+                {
+                    model.Users.Add(user.UserName);
+                }      
+            }
+
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // EditRole - || Post ||
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id); // Find Role    
+            if (role == null) // If role dont exists
+            {
+                ViewBag.ErrorMessage = $"Role with ID = {model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else // If Ok
+            {
+                role.Name = model.RoleName;
+                var result = await roleManager.UpdateAsync(role);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+           
+        }
     }
 }
---
+++ Web/Models/EditRoleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class EditRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+
+
+         // ||Constructor||
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+    }
+}
